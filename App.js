af21dac82c47981564bcde98b466293b3aae69ea
Oleg Pudeyev <oleg@bsdpower.com>
2017-12-25 17:35:27 -0500

    Text input and debouncing

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {TextInput} from 'react-native'
+import debounce from 'debounce'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      query: '',
+      results: [],
+    }
+    
+    this.fetch = debounce(this.fetch, 1000)
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>
+          Giphy Search
+        </Text>
+        
+        <TextInput style={{height: 40}}
+          placeholder='Search...'
+          onChangeText={this.query_did_change.bind(this)}
+        />
+        
+        {this.state.query ?
+          <Text>
+            Gifs for {this.state.query}
+          </Text>
+        : null}
+        
+        {this.state.results.map((result, index) => (
+          <Text key={index}>
+            {result}
+          </Text>
+        ))}
       </View>
     );
+  }
+  
+  query_did_change(text) {
+    this.setState({query: text})
+    this.fetch(text)
+  }
+  
+  fetch(query) {
+    this.setState({
+      results: [...this.state.results, query],
+    })
   }
 }
 
